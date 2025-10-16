7bc1df13847bf1cecd65f0ed4767ea39f98c9731
Valeria <yomeodio@gmail.com>
2025-10-16 18:17:31 -0500

    Consultas cl20 completadas

--- consultas.sql
+++ consultas.sql
@@ -80,3 +80,91 @@ HAVING AVG([Duración de Sesión en Horas en Promedio]) > 3;
 
 
 --Clase dia Jueves 16/10/2025
+-- Tabla complementaria 
+CREATE TABLE GeneroJuegoInfo (
+    [Género de Juego] NVARCHAR(100) PRIMARY KEY,
+    [Tipo] NVARCHAR(50),
+    [Popularidad] INT
+);
+
+INSERT INTO GeneroJuegoInfo VALUES
+('Acción', 'Competitivo', 90),
+('Aventura', 'Narrativo', 80),
+('Estrategia', 'Mental', 75),
+('Deportes', 'Realista', 85);
+
+-- INNER JOIN para combinar tablas
+SELECT                            -- Selecciona columnas específicas
+    d.[ID de Jugador],            -- Columna de la tabla principal- d
+    d.[Género de Juego],          -- Columna de la tabla principal -d
+    g.[Tipo]                     -- Columna de la tabla relacionada  -g
+FROM ComportamientoOnlineDatos AS d           -- Alias para la tabla principal
+INNER JOIN GeneroJuegoInfo AS g               -- Alias para la tabla relacionada
+    ON d.[Género de Juego] = g.[Género de Juego];   -- Condición de unión
+
+-- LEFT JOIN para incluir todos los jugadores
+--TODOS LOS JUGADORES / REGISTROS DE LA TABLA IZQUIERDA Y NO IMPORTA SI HAY O NO COINCIDENCIA EN LA TABLA DERECHA
+SELECT
+    d.[ID de Jugador],
+    d.[Género de Juego],
+    g.[Tipo],
+    g.[Popularidad]
+FROM ComportamientoOnlineDatos AS d
+LEFT JOIN GeneroJuegoInfo AS g
+    ON d.[Género de Juego] = g.[Género de Juego];   
+
+-- Subconsultas
+-- Jugadores mayores al promedio de edad
+SELECT 
+    [ID de Jugador],
+    [Edad],
+    [Ubicación]
+FROM ComportamientoOnlineDatos
+WHERE [Edad] > (
+    SELECT AVG([Edad]) FROM ComportamientoOnlineDatos
+);
+-- Jugadores que han hecho compras superiores al promedio
+SELECT 
+    [ID de Jugador],
+    [Compra en Juego]   
+FROM ComportamientoOnlineDatos
+WHERE [Compra en Juego] > (
+    SELECT AVG([Compra en Juego]) FROM ComportamientoOnlineDatos
+);
+
+-- VISTAs
+--jugadores menrores a 16 años
+CREATE VIEW JugadoresMenores16 AS
+SELECT 
+    [ID de Jugador],
+    [Edad],
+    [Nivel de Jugador]
+FROM ComportamientoOnlineDatos
+WHERE [Edad] < 16;
+
+-- Consultar la vista
+SELECT * FROM JugadoresMenores16;
+
+--ista juafores con nivel menor a 5
+CREATE VIEW JugadoresNivel1 AS
+SELECT
+    [ID de Jugador],
+    [Nivel de Jugador],
+    [Duración de Sesión en Horas en Promedio]
+FROM ComportamientoOnlineDatos
+WHERE [Nivel de Jugador] = 1;
+
+-- Consultar la vista
+SELECT * FROM JugadoresNivel1;
+
+--Jugadores con nivel igual a 3 y solo los primeros 20
+CREATE VIEW JugadoresNivel3 AS
+SELECT TOP 20
+    [ID de Jugador],
+    [Nivel de Jugador],
+    [Duración de Sesión en Horas en Promedio]   
+FROM ComportamientoOnlineDatos
+WHERE [Nivel de Jugador] = 3;
+
+-- Consultar la vista
+SELECT * FROM JugadoresNivel3;
